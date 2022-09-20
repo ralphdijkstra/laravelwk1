9edbede3792c799eb5bff6b8a062a074bd35a83a
BuildTools <unconfigured@null.spigotmc.org>
2022-09-20 20:13:59 +0200

    navbar2

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -16,38 +16,34 @@
             <div class="flex justify-between">
                 <div class="flex space-x-7">
                     <div>
-                        <!-- Website Logo -->
                         <a href="#" class="flex items-center py-4">
                             <div class="w-52">
                                 <img class="object-contain" src="logo.png" />
                             </div>
                         </a>
                     </div>
-                    <!-- Primary Navbar items -->
                     <div class="hidden md:flex items-center space-x-5 text-xl">
                         <a href="" class="py-4 px-2 text-red-500 font-semibold ">Home</a>
-                        <a href="" class="py-4 px-2 text-black font-semibold hover:text-red-500 transition duration-300">Services</a>
-                        <a href="" class="py-4 px-2 text-black font-semibold hover:text-red-500 transition duration-300">About</a>
-                        <a href="" class="py-4 px-2 text-black font-semibold hover:text-red-500 transition duration-300">Contact Us</a>
+                        <a href="" class="py-4 px-2 text-black font-semibold hover:text-red-500 transition duration-300">Vestigingen</a>
+                        <a href="" class="py-4 px-2 text-black font-semibold hover:text-red-500 transition duration-300">Producten</a>
+                        <a href="" class="py-4 px-2 text-black font-semibold hover:text-red-500 transition duration-300">Vacatures</a>
                     </div>
                 </div>
-                <!-- Mobile menu button -->
                 <div class="md:hidden flex items-center">
                     <button class="outline-none mobile-menu-button">
-                        <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 " x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg class=" w-6 h-6 text-black hover:text-red-500 " x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                             <path d="M4 6h16M4 12h16M4 18h16"></path>
                         </svg>
                     </button>
                 </div>
             </div>
         </div>
-        <!-- mobile menu -->
         <div class="hidden mobile-menu">
             <ul class="">
-                <li class="active"><a href="index.html" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-                <li><a href="#services" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-                <li><a href="#about" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
-                <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
+                <li class="active"><a href="index.html" class="block text-sm px-2 py-4 text-white bg-red-500 font-semibold">Home</a></li>
+                <li><a href="#services" class="block text-sm px-2 py-4 hover:bg-red-500 transition duration-300">Services</a></li>
+                <li><a href="#about" class="block text-sm px-2 py-4 hover:bg-red-500 transition duration-300">About</a></li>
+                <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-red-500 transition duration-300">Contact Us</a></li>
             </ul>
         </div>
         <script>
