cc245b81c8fabec4b90e97a67d42b6659fbc924f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-04 15:51:13 +0200

    version 1.1

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -25,7 +25,7 @@
         </div>
 
         <svg xmlns="http://www.w3.org/2000/svg" id="menu-button" class="h-6 w-6 cursor-pointer md:hidden block"
-            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            fill="none" viewBox="0 0 24 24" stroke="white">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
 
